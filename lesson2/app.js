951697e2433cf36db751c450fe58426fd3e1b036
steffipereira <steffipereira23@gmail.com>
2020-01-14 03:39:32 +0530

    conditional loops

--- lesson2/app.js
+++ lesson2/app.js
@@ -4,7 +4,43 @@ new Vue({
         age:30,
         a:0,
         b: 0,
-        available : false
+        available : false,
+        success: false,
+        error: false,
+        members: [
+            {
+              "name": "Molecule Man",
+              "age": 29,
+              "secretIdentity": "Dan Jukes",
+              "powers": [
+                "Radiation resistance",
+                "Turning tiny",
+                "Radiation blast"
+              ]
+            },
+            {
+              "name": "Madame Uppercut",
+              "age": 39,
+              "secretIdentity": "Jane Wilson",
+              "powers": [
+                "Million tonne punch",
+                "Damage resistance",
+                "Superhuman reflexes"
+              ]
+            },
+            {
+              "name": "Eternal Flame",
+              "age": 1000000,
+              "secretIdentity": "Unknown",
+              "powers": [
+                "Immortality",
+                "Heat Immunity",
+                "Inferno",
+                "Teleportation",
+                "Interdimensional travel"
+              ]
+            }
+          ]
     },
     methods: {
 
